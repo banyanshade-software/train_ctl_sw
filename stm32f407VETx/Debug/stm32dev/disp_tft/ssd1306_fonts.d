3097ded70c97dcd10a7623746971660ee6927e00
Daniel Braun <braun.daniel@free.fr>
2021-10-16 18:04:51 +0200

    some cleanup

--- stm32f407VETx/Debug/stm32dev/disp_tft/ssd1306_fonts.d
+++
@@ -1,8 +0,0 @@
-stm32dev/disp_tft/ssd1306_fonts.o: \
- /Users/danielbraun/devel/train/sw/stm32dev/disp_tft/ssd1306_fonts.c \
- /Users/danielbraun/devel/train/sw/stm32dev/disp_tft/ssd1306_fonts.h \
- /Users/danielbraun/devel/train/sw/stm32dev/disp_tft/ssd1306_conf.h
-
-/Users/danielbraun/devel/train/sw/stm32dev/disp_tft/ssd1306_fonts.h:
-
-/Users/danielbraun/devel/train/sw/stm32dev/disp_tft/ssd1306_conf.h:

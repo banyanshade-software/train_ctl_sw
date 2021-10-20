616fed08dd8169a557a4ab80f995e7343406eb35
Daniel Braun <braun.daniel@free.fr>
2021-10-20 23:27:16 +0200

    adding LED light control with unit test

---
+++ trainctl/leds/led.c
@@ -0,0 +1,9 @@
+//
+//  led.c
+//  train_throttle
+//
+//  Created by Daniel BRAUN on 20/10/2021.
+//  Copyright © 2021 Daniel BRAUN. All rights reserved.
+//
+
+#include "led.h"
---
+++ trainctl/leds/led.h
@@ -0,0 +1,14 @@
+//
+//  led.h
+//  train_throttle
+//
+//  Created by Daniel BRAUN on 20/10/2021.
+//  Copyright © 2021 Daniel BRAUN. All rights reserved.
+//
+
+#ifndef led_h
+#define led_h
+
+#include <stdio.h>
+
+#endif /* led_h */

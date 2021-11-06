db12c418f8e6517a5948b9fdc7206918bb0605d2
Daniel Braun <braun.daniel@free.fr>
2021-11-06 11:47:36 +0100

    adding SvgGen

---
+++ mac/SvgGen/SvgGen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SvgGen
+//
+//  Created by Daniel BRAUN on 06/11/2021.
+//  Copyright © 2021 Daniel BRAUN. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate, WKUIDelegate, WKNavigationDelegate>
+
+@property (nonatomic, weak) IBOutlet WKWebView *ctoWebView;
+
+@end
+
---
+++ mac/SvgGen/SvgGen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SvgGen
+//
+//  Created by Daniel BRAUN on 06/11/2021.
+//  Copyright © 2021 Daniel BRAUN. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}

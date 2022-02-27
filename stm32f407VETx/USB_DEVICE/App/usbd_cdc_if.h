6a05f9a97c64d4f334eb4ce7466c1781d84ccb47
Daniel Braun <braun.daniel@free.fr>
2022-02-27 19:50:01 +0100

    removed some warning

--- stm32f407VETx/USB_DEVICE/App/usbd_cdc_if.h
+++ stm32f407VETx/USB_DEVICE/App/usbd_cdc_if.h
@@ -54,6 +54,8 @@
 /* USER CODE BEGIN EXPORTED_DEFINES */
 /* Define size for the receive and transmit buffer over CDC */
 /* It's up to user to redefine and/or remove those define */
+#undef APP_RX_DATA_SIZE
+#undef APP_TX_DATA_SIZE
 #define APP_RX_DATA_SIZE  2048
 #define APP_TX_DATA_SIZE  2048
 

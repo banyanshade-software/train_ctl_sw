16f2f56ca5c39423baf76b3fab9f48c68638ce6e
Daniel Braun <braun.daniel@free.fr>
2022-03-05 09:33:16 +0100

    rename oscilo.h to oscillo.h

--- trainctl/stm32/ina3221/ina3221.c
+++ trainctl/stm32/ina3221/ina3221.c
@@ -6,7 +6,6 @@
  */
 
 
-#include <oscillo/oscillo.h>
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_gpio.h"
 #include "cmsis_os2.h"
@@ -18,6 +17,7 @@
 
 #include "misc.h"
 #include "../../msg/trainmsg.h"
+#include "../../oscillo/oscillo.h""
 
 
 #ifndef BOARD_HAS_INA3221

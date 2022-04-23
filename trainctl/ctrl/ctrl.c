66552e03cb11ae671c68a464892ec9487be1f9d1
Daniel Braun <braun.daniel@free.fr>
2022-04-23 16:14:36 +0200

    dont start auto train

--- trainctl/ctrl/ctrl.c
+++ trainctl/ctrl/ctrl.c
@@ -582,7 +582,7 @@ void ctrl_run_tick(_UNUSED_ uint32_t notif_flags, uint32_t tick, _UNUSED_ uint32
     }
     
 #ifndef TRAIN_SIMU
-    if ((1)) {
+    if ((0)) {
         static int nsk=0;
         nsk++;
         if ((trctl[0]._mode != train_auto) && (nsk==100)) {

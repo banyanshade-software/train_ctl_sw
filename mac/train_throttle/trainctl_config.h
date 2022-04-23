fa2d72819a02393215430d118073543bf69cca5e
Daniel Braun <braun.daniel@free.fr>
2022-04-24 00:40:25 +0200

    update mac for new usb protocol

---
+++ mac/train_throttle/trainctl_config.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  train_throttle
+//
+//  Created by danielbraun on 23/04/2022.
+//  Copyright © 2022 Daniel BRAUN. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */

7431f7921921fd16f250fa66d9983796016d60db
Daniel Braun <braun.daniel@free.fr>
2022-04-24 18:56:08 +0200

    cleanup for dispatcher and switcher

---
+++ stm32f103_switcher/Core/Inc/trainctl_config.h
@@ -0,0 +1,22 @@
+/*
+ * trainctl_config.h
+ *
+ *  Created on: Nov 23, 2020
+ *      Author: danielbraun
+ */
+
+#ifndef INC_TRAINCTL_CONFIG_H_
+#define INC_TRAINCTL_CONFIG_H_
+
+
+//#define TRN_BOARD_MAIN
+//#define TRN_BOARD_DISPATCHER
+#define TRN_BOARD_SWITCHER
+//#define TRN_BOARD_UI
+// #define TRN_BOARD_ZERO
+
+#include "boards_def.h"
+
+
+
+#endif /* INC_TRAINCTL_CONFIG_H_ */

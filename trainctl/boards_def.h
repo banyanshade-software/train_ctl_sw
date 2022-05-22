56139af1757d659a66dc624a62973257d1606010
Daniel Braun <braun.daniel@free.fr>
2022-05-22 21:10:48 +0200

    multiple display addresses in ssd1306

--- trainctl/boards_def.h
+++ trainctl/boards_def.h
@@ -107,7 +107,8 @@
 #define BOARD_HAS_TRKPLN
 #define BOARD_HAS_OSCILLO
 
-#define SSD1306_I2C_PORT (hi2c3)
+#define SSD1306_I2C_PORTS_DECL extern I2C_HandleTypeDef hi2c3
+#define SSD1306_I2C_PORTS { &hi2c3 }
 #define INA3221_I2C_PORT (hi2c1)
 
 /*
@@ -197,7 +198,7 @@
 //#define BOARD_HAS_TRKPLN
 // #define BOARD_CAN_BE_MASTER
 
-#define SSD1306_I2C_PORT        hi2c1
+#define SSD1306_I2C_PORTS        { &hi2c1 }
 
 /*
  * number of local block per board
@@ -241,7 +242,7 @@
 //#define BOARD_HAS_TRKPLN
 // #define BOARD_CAN_BE_MASTER
 
-#define SSD1306_I2C_PORT        hi2c1
+#define SSD1306_I2C_PORTS        { &hi2c1}
 
 /*
  * number of local block per board

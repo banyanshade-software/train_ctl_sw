c02f08e36082c2b580175b6c74d0fccce0fdff8e
banyanshade-software <braun.daniel@free.fr>
2022-06-27 23:12:59 +0200

    fix push button have inverted level

--- trainctl/IHM/button.c
+++ trainctl/IHM/button.c
@@ -13,6 +13,7 @@
 uint8_t ihm_poll_button(ihm_button_t *b, uint32_t tick)
 {
 	int v = HAL_GPIO_ReadPin(b->def->port, b->def->pin);
+	v = !v; // button state : pressed = 1 (gpio = gnd)
 	if (v && ! b->laststate) {
 		b->tpush = tick;
 		b->laststate = 1;

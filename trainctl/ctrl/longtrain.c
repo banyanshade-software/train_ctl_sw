79c8d0b4ca03f84265914c88a3b87a319e955237
banyanshade-software <braun.daniel@free.fr>
2022-12-20 20:57:13 +0100

    moved long train functions to longtrain.c

---
+++ trainctl/ctrl/longtrain.c
@@ -0,0 +1,9 @@
+//
+//  longtrain.c
+//  train_throttle
+//
+//  Created by Daniel Braun on 20/12/2022.
+//  Copyright © 2022 Daniel BRAUN. All rights reserved.
+//
+
+#include "longtrain.h"
---
+++ trainctl/ctrl/longtrain.h
@@ -0,0 +1,14 @@
+//
+//  longtrain.h
+//  train_throttle
+//
+//  Created by Daniel Braun on 20/12/2022.
+//  Copyright © 2022 Daniel BRAUN. All rights reserved.
+//
+
+#ifndef longtrain_h
+#define longtrain_h
+
+#include <stdio.h>
+
+#endif /* longtrain_h */

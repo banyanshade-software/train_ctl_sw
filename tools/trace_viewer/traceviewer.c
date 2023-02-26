a24d1cf121e236c11cad2748c722cfdd5da812c2
banyanshade-software <braun.daniel@free.fr>
2023-02-26 16:39:27 +0100

    update display

--- tools/trace_viewer/traceviewer.c
+++ tools/trace_viewer/traceviewer.c
@@ -49,13 +49,17 @@ int main(int argc, char **argv)
 {
     int nlines = 0;
     int nskip = 0;
+    int noch = 0;
     char *fields[6];
     char line[1024];
-    char taskview[MAXTASK+1]; // max 16 tasks
-    char taskstate[MAXTASK+1];
+    char taskview[2*MAXTASK+1]; // max 16 tasks
+    char taskstate[2*MAXTASK+1];
     memset(taskstate, ' ', sizeof(taskstate));
-    taskstate[MAXTASK]='\0';
-    memcpy(taskview, taskstate, MAXTASK+1);
+    taskstate[2*MAXTASK]='\0';
+    memcpy(taskview, taskstate, 2*MAXTASK+1);
+    unsigned long lastcycle = 0;
+    double lastts = 0.0;
+
     while (fgets(line, 1024, stdin)) {
         //printf("---- %s\n", line);
         int n = extract_fields(line, fields, 6);
@@ -64,10 +68,17 @@ int main(int argc, char **argv)
             continue;
         }
         nlines++;
+
+        if (strcmp(fields[1],"ITM Port 1"))  {
+            noch++;
+            continue;
+        }
+
         //printf("n=%d num %s port %s\n", n, fields[0], fields[1]);
 
         //"   33";"ITM Port 1";"260";"107404534";"2,237594 s";"Timestamp delayed. Packet delayed. "
         double ts;
+        double dts = 0.0;
         if (fields[4][0]=='?') {
             ts = -1;
         } else {
@@ -77,35 +88,42 @@ int main(int argc, char **argv)
                 if (','==*p) *p = '.';
             }
             ts = atof(fields[4]);
+            if (lastts) {
+                dts = ts - lastts;
+            }
+            lastts = ts; 
         }
         unsigned long cycle = atoi(fields[3]);
+        unsigned long dcycle = 0;
+        if (lastcycle) dcycle = cycle-lastcycle;
+        lastcycle = cycle;
         unsigned int evt = atoi(fields[2]);
         unsigned int evt_type = evt >> 8;
         unsigned int task = evt & 0xFF;
         char *evts = "??";
         int tick = 0;
         
-        memcpy(taskview, taskstate, MAXTASK+1);
+        memcpy(taskview, taskstate, 2*MAXTASK+1);
         switch (evt_type) {
-        case MONITOR_SW_IN:     evts = "SCHED IN   "; taskstate[task]='X'; taskview[task]='I'; break;
-        case MONITOR_SW_OUT:    evts = "SCHED OUT  "; taskstate[task]=' '; taskview[task]='-'; break;
+        case MONITOR_SW_IN:     evts = "SCHED IN   "; taskstate[2*task]=(task==4) ? 'x' : 'X'; taskview[2*task]='I'; break;
+        case MONITOR_SW_OUT:    evts = "SCHED OUT  "; taskstate[2*task]='|'; taskview[2*task]='-'; break;
         case MONITOR_TICK:      evts = "TICK"; tick = 1; break;
-        case MONITOR_READY:     evts = "READY      "; taskview[task]='R'; break;
-        case MONITOR_NOTIF_WB:  evts = "NOTIF W BLK"; taskview[task]='W'; break;
-        case MONITOR_NOTIF_W:   evts = "SCHED W    "; taskview[task]='W'; break;
-        case MONITOR_DELAY_U:   evts = "DELAY UNTIL"; taskview[task]='D'; break;
-        case MONITOR_DELAY:     evts = "DELAY      "; taskview[task]='D'; break;
+        case MONITOR_READY:     evts = "READY      "; taskview[2*task]='R'; break;
+        case MONITOR_NOTIF_WB:  evts = "NOTIF W BLK"; taskview[2*task]='W'; break;
+        case MONITOR_NOTIF_W:   evts = "SCHED W    "; taskview[2*task]='W'; break;
+        case MONITOR_DELAY_U:   evts = "DELAY UNTIL"; taskview[2*task]='D'; break;
+        case MONITOR_DELAY:     evts = "DELAY      "; taskview[2*task]='D'; break;
         case MONITOR_NOTIF:     evts = "NOTIF snd  "; break;
         case MONITOR_NOTIF_ISR: evts = "NOTIF s ISR"; break;
         default : break;
         }
-        if (ts>=0) printf("%12.6f : ", ts);
-        else printf("             : ");
+        if (ts>=0) printf("%12.6f [%0.4f] : ", ts, dts);
+        else printf("                      : ");
         if (tick) {
-            printf("%8lu %s ------------------------------------ systick\n", cycle, taskview);
+            printf("%8lu [%6lu] %s ------------------------------------ systick\n", cycle, dcycle, taskview);
         } else {
-            printf("%8lu %s %s TASK%d\n", cycle, taskview, evts, task);
+            printf("%8lu [%6lu] %s %s TASK%d\n", cycle, dcycle, taskview, evts, task);
         }
     }
-    printf("scanned %d, skipped %d\n", nlines, nskip);
+    printf("scanned %d, skipped %d, other ch %d\n", nlines, nskip, noch);
 }

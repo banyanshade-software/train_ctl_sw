8330525a8a4368c341696b234c7ea5679c944476
banyanshade-software <braun.daniel@free.fr>
2023-03-09 00:24:17 +0100

    fix compile problem with SvgGen

--- trainctl/topology/topologyP.h
+++ trainctl/topology/topologyP.h
@@ -48,9 +48,10 @@ typedef conf_topology_t topo_lsblk_t;
 
 const topo_lsblk_t *topology_get_sblkd(int lsblk);
 
+#ifndef TOPOLOGY_SVG
 
 void occupency_turnout_release_for_train_canton(int train, xblkaddr_t canton);
 void occupency_clear_turnouts(void);
-
+#endif
 
 #endif /* topologyP_h */

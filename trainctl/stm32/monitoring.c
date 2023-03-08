5e4d6e34af0d6b06fad23748869cdb8b386bd95d
banyanshade-software <braun.daniel@free.fr>
2023-03-09 00:25:30 +0100

    only include code if ENABLE_TRACES is defined

--- trainctl/stm32/monitoring.c
+++ trainctl/stm32/monitoring.c
@@ -9,15 +9,26 @@
 #include "cmsis_os.h"
 #include "monitoring.h"
 
+#ifdef ENABLE_TRACES
+
 extern void itmTraceSend(int ch, uint8_t type,  uint8_t data)
 {
 	if (((ITM->TCR & ITM_TCR_ITMENA_Msk) != 0UL) &&      /* ITM enabled */
 	    ((ITM->TER & (1<<ch)           ) != 0UL)   )  {   /* ITM Port #0 enabled */
 
+		int n=0;
 		while (ITM->PORT[ch].u32 == 0UL)  {
+			if (n++>TRACE_MAW_WAIT) return;
 			__NOP();
 		}
 		uint16_t v = (type<<8) | data;
 		ITM->PORT[ch].u16 = v;
 	}
 }
+
+void itmTraceTaskCreate(int tcbnnum, int tasknum, char *taskname)
+{
+	(void)tcbnnum; (void) tasknum; (void)taskname;
+}
+
+#endif

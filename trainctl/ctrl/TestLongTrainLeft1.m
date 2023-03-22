ab66033d91efcf791dbd1214946933116f9b775b
banyanshade-software <braun.daniel@free.fr>
2023-03-22 18:50:23 +0100

    fix turnout in c3autoP.c

---
+++ trainctl/ctrl/TestLongTrainLeft1.m
@@ -0,0 +1,37 @@
+//
+//  TestLongTrainLeft1.m
+//  trainctlTests
+//
+//  Created by Daniel Braun on 21/03/2023.
+//  Copyright © 2023 Daniel BRAUN. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestLongTrainLeft1 : XCTestCase
+
+@end
+
+@implementation TestLongTrainLeft1
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

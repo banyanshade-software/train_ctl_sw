be1de0087de993494791974da870acee04fd6785
banyanshade-software <braun.daniel@free.fr>
2023-05-04 20:36:16 +0200

    add G4 in headers

--- trainctl/config/conf_led.h
+++ trainctl/config/conf_led.h
@@ -19,14 +19,26 @@
 
 
 #ifndef TRAIN_SIMU
-#ifdef STM32_F4
+
+#if defined(STM32F4)
 #include "stm32f4xx_hal.h"
-#else
+
+#elif defined(STM32G4)
+#include "stm32g4xx_hal.h"
+
+#elif defined(STM32F1)
 #include "stm32f1xx_hal.h"
+
+#else
+#error no board hal
 #endif
+
+
 #else
 typedef void *GPIO_TypeDef;
 #endif
+
+
 
 
 

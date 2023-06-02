a6899271abdd86d7712e9260b8e3ce4851b99915
banyanshade-software <braun.daniel@free.fr>
2023-06-02 22:47:39 +0200

    openocdF4 ok

--- openocdF4.cmd
+++ openocdF4.cmd
@@ -4,11 +4,12 @@ P=/Users/danielbraun/devel/train/_sw_lt7
 C=/opt/openocd
 
 
-openocd -f myocd.cfg \
-    "-f" "$P/stm32f4_mainV4.cfg" \
+$C/bin/openocd -f myocd.cfg \
+    "-f" "$P/stm32f4_mainV4/stm32f4_mainV4.cfg" \
 	"-s" "$P/stm32f4_mainV4" \
     	"-s" "$C/st_scripts" \
 	"-c" "gdb_report_data_abort enable"\
 	"-c" "gdb_port 3333" \
-	"-c" "tcl_port 6666" "-c" "telnet_port 4444" \
-	-d -l ocd.log
+	"-c" "tcl_port 6666" "-c" "telnet_port 4444" 
+
+#	-d -l ocd.log

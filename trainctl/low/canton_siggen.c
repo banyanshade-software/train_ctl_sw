0032d5091769873513b3598248fa4a99de729368
banyanshade-software <braun.daniel@free.fr>
2024-01-20 12:21:25 +0100

    set_oneshot changed to several freq

--- trainctl/low/canton_siggen.c
+++ trainctl/low/canton_siggen.c
@@ -56,17 +56,100 @@ static void gen_signal(int t, int nbuf)
 
 // -----------------------------------------------------------------------
 
-static void set_oneshot(int timernum, const conf_canton_t *cconf)
+
+static void set_oneshot(int cidx, _UNUSED_ int timernum,  const conf_canton_t *cconf, canton_vars_t *cvars)
 {
-	TIM_HandleTypeDef *pwm_timer = CantonTimerHandles[timernum];
+	//TIM_HandleTypeDef *pwm_timer = CantonTimerHandles[timernum];
 	//HAL_StatusTypeDef rc = HAL_TIM_OnePulse_Init(pwm_timer, TIM_OPMODE_SINGLE);
+
+	int freqhz = get_pwm_freq();
+
+#define TFREQ(_f) do { \
+	set_pwm_freq(_f, 1); \
+	canton_set_pwm(cidx, cconf, cvars, 1, 20); \
+	osDelay(20); \
+	canton_set_pwm(cidx, cconf, cvars, 0, 0); \
+	osDelay(20); \
+} while (0)
+
+	//int ms = 1*1000/freqhz;
+	itm_debug2(DBG_DETECT, "st-oneshot", cidx, 0);
+
+	TFREQ(250);
+	TFREQ(500);
+	TFREQ(1000);
+	TFREQ(2000);
+	TFREQ(5000);
+	TFREQ(10000);
+	TFREQ(20000);
+
+	canton_set_pwm(cidx, cconf, cvars, 0, 0); \
+
+	itm_debug1(DBG_DETECT, "end-onesh", cidx);
+	set_pwm_freq(freqhz, 0); \
+
+#if 0
+	HAL_TIM_Base_Stop(pwm_timer);
 
 	TIM_SlaveConfigTypeDef sSlaveConfig = {0};
 	sSlaveConfig.SlaveMode = TIM_SLAVEMODE_DISABLE;
 	sSlaveConfig.InputTrigger = TIM_TS_ITR0;
 	HAL_TIM_SlaveConfigSynchro(pwm_timer, &sSlaveConfig);
 
-	// CounterMode
+	pwm_timer->Init.Prescaler = 1199;
+	pwm_timer->Init.CounterMode = TIM_COUNTERMODE_UP;
+	pwm_timer->Init.Period = 200;
+	pwm_timer->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	pwm_timer->Init.RepetitionCounter = 0;
+	pwm_timer->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+
+	if (HAL_TIM_Base_Init(pwm_timer) != HAL_OK) {
+		Error_Handler();
+	}
+	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	if (HAL_TIM_ConfigClockSource(pwm_timer, &sClockSourceConfig) != HAL_OK) {
+		Error_Handler();
+	}
+    if (HAL_TIM_PWM_Init(pwm_timer) != HAL_OK)  {
+	     Error_Handler();
+	}
+	if (HAL_TIM_OnePulse_Init(pwm_timer, TIM_OPMODE_SINGLE) != HAL_OK) {
+	      Error_Handler();
+	}
+	TIM_MasterConfigTypeDef sMasterConfig = {0};
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_ENABLE;
+	if (HAL_TIMEx_MasterConfigSynchronization(pwm_timer, &sMasterConfig) != HAL_OK) {
+		Error_Handler();
+	}
+	TIM_OC_InitTypeDef sConfigOC = {0};
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = 0;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+	sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+	if (HAL_TIM_PWM_ConfigChannel(pwm_timer, &sConfigOC, cconf->ch0) != HAL_OK) {
+		Error_Handler();
+	}
+
+	TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+	sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+	sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+	sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+	sBreakDeadTimeConfig.DeadTime = 0;
+	sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+	sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+	sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+
+	if (HAL_TIMEx_ConfigBreakDeadTime(pwm_timer, &sBreakDeadTimeConfig) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_TIM_MspPostInit(pwm_timer);
+#if 0
 	pwm_timer->Instance->CR1 &= ~(TIM_CR1_DIR | TIM_CR1_CMS);
 	pwm_timer->Instance->CR1 |= TIM_COUNTERMODE_UP;
 	/* Reset the OPM Bit */
@@ -74,17 +157,48 @@ static void set_oneshot(int timernum, const conf_canton_t *cconf)
 
 	  /* Configure the OPM Mode */
 	pwm_timer->Instance->CR1 |= TIM_OPMODE_SINGLE;
+#endif
+#define CNT_OFF 0
+#define CNT_ON(_v) (_v)
 
-	HAL_TIM_OnePulse_Start(pwm_timer, cconf->ch1);
+	pwm_timer->Instance->CCR1 = CNT_OFF;
+	pwm_timer->Instance->CCR2 = CNT_OFF;
+	pwm_timer->Instance->CCR3 = CNT_OFF;
+	pwm_timer->Instance->CCR4 = CNT_OFF;
+	int t=200;
+
+	switch (cconf->ch0) {
+	case TIM_CHANNEL_1:
+		pwm_timer->Instance->CCR1 = CNT_ON(t);
+		break;
+	case TIM_CHANNEL_2:
+		itm_debug1(DBG_LOWCTRL|DBG_TIM, "CH2/CCR1", 0);
+		pwm_timer->Instance->CCR2 = CNT_ON(t);
+		break;
+	case TIM_CHANNEL_3:
+		itm_debug1(DBG_LOWCTRL|DBG_TIM, "CH3/CCR1", 0);
+		pwm_timer->Instance->CCR3 = CNT_ON(t);
+		break;
+	case TIM_CHANNEL_4:
+		itm_debug1(DBG_LOWCTRL|DBG_TIM, "CH4/CCR1", 0);
+		pwm_timer->Instance->CCR4 = CNT_ON(t);
+		break;
+	default:
+		break;
+	}
+
+	HAL_TIM_OnePulse_Start(pwm_timer, cconf->ch0);
+	HAL_TIM_Base_Start(pwm_timer);
 
 	//if (rc) FatalError("TIMd", "time dirac", Error_Abort);
+#endif
 }
 
 void start_signal_dirac(int cidx, const conf_canton_t *cconf,  canton_vars_t *cvars)
 {
 	itm_debug1(DBG_DETECT, "C/oneshot", cidx);
 	canton_set_volt(cidx, cconf, cvars, 7);
-	set_oneshot(cconf->pwm_timer_num, cconf);
+	set_oneshot(cidx, cconf->pwm_timer_num, cconf, cvars);
 	//canton_set_pwm(cidx, cconf, cvars, 1 /*sdir*/, 10 /*duty*/);
 }
 

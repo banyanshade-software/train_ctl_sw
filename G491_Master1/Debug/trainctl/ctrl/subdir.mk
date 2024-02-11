0c7dd859e245ff2983e07b71e70d22312d4b1c10
banyanshade-software <braun.daniel@free.fr>
2024-02-11 09:15:00 +0100

    updated project files

--- G491_Master1/Debug/trainctl/ctrl/subdir.mk
+++ G491_Master1/Debug/trainctl/ctrl/subdir.mk
@@ -11,10 +11,11 @@ C_SRCS += \
 /Users/danielbraun/devel/train/sw/trainctl/ctrl/ctrl.c \
 /Users/danielbraun/devel/train/sw/trainctl/ctrl/ctrlLT.c \
 /Users/danielbraun/devel/train/sw/trainctl/ctrl/ctrlP.c \
-/Users/danielbraun/devel/train/sw/trainctl/ctrl/detect_loco.c \
+/Users/danielbraun/devel/train/sw/trainctl/ctrl/locomotives.c \
 /Users/danielbraun/devel/train/sw/trainctl/ctrl/longtrain.c \
 /Users/danielbraun/devel/train/sw/trainctl/ctrl/longtrain4.c \
-/Users/danielbraun/devel/train/sw/trainctl/ctrl/trace_train.c 
+/Users/danielbraun/devel/train/sw/trainctl/ctrl/trace_train.c \
+/Users/danielbraun/devel/train/sw/trainctl/ctrl/train2loco.c 
 
 OBJS += \
 ./trainctl/ctrl/c3autoP.o \
@@ -23,10 +24,11 @@ OBJS += \
 ./trainctl/ctrl/ctrl.o \
 ./trainctl/ctrl/ctrlLT.o \
 ./trainctl/ctrl/ctrlP.o \
-./trainctl/ctrl/detect_loco.o \
+./trainctl/ctrl/locomotives.o \
 ./trainctl/ctrl/longtrain.o \
 ./trainctl/ctrl/longtrain4.o \
-./trainctl/ctrl/trace_train.o 
+./trainctl/ctrl/trace_train.o \
+./trainctl/ctrl/train2loco.o 
 
 C_DEPS += \
 ./trainctl/ctrl/c3autoP.d \
@@ -35,10 +37,11 @@ C_DEPS += \
 ./trainctl/ctrl/ctrl.d \
 ./trainctl/ctrl/ctrlLT.d \
 ./trainctl/ctrl/ctrlP.d \
-./trainctl/ctrl/detect_loco.d \
+./trainctl/ctrl/locomotives.d \
 ./trainctl/ctrl/longtrain.d \
 ./trainctl/ctrl/longtrain4.d \
-./trainctl/ctrl/trace_train.d 
+./trainctl/ctrl/trace_train.d \
+./trainctl/ctrl/train2loco.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -54,7 +57,7 @@ trainctl/ctrl/ctrlLT.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/ctrlLT.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G491xx -c -I"/Users/danielbraun/devel/train/sw/trainctl" -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -Wswitch-default -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 trainctl/ctrl/ctrlP.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/ctrlP.c trainctl/ctrl/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G491xx -c -I"/Users/danielbraun/devel/train/sw/trainctl" -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -Wswitch-default -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
-trainctl/ctrl/detect_loco.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/detect_loco.c trainctl/ctrl/subdir.mk
+trainctl/ctrl/locomotives.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/locomotives.c trainctl/ctrl/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G491xx -c -I"/Users/danielbraun/devel/train/sw/trainctl" -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -Wswitch-default -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 trainctl/ctrl/longtrain.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/longtrain.c trainctl/ctrl/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G491xx -c -I"/Users/danielbraun/devel/train/sw/trainctl" -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -Wswitch-default -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
@@ -62,11 +65,13 @@ trainctl/ctrl/longtrain4.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/long
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G491xx -c -I"/Users/danielbraun/devel/train/sw/trainctl" -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -Wswitch-default -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 trainctl/ctrl/trace_train.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/trace_train.c trainctl/ctrl/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G491xx -c -I"/Users/danielbraun/devel/train/sw/trainctl" -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -Wswitch-default -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+trainctl/ctrl/train2loco.o: /Users/danielbraun/devel/train/sw/trainctl/ctrl/train2loco.c trainctl/ctrl/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G491xx -c -I"/Users/danielbraun/devel/train/sw/trainctl" -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -O0 -ffunction-sections -fdata-sections -Wall -Wswitch-default -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-trainctl-2f-ctrl
 
 clean-trainctl-2f-ctrl:
-	-$(RM) ./trainctl/ctrl/c3autoP.cyclo ./trainctl/ctrl/c3autoP.d ./trainctl/ctrl/c3autoP.o ./trainctl/ctrl/c3autoP.su ./trainctl/ctrl/cautoP.cyclo ./trainctl/ctrl/cautoP.d ./trainctl/ctrl/cautoP.o ./trainctl/ctrl/cautoP.su ./trainctl/ctrl/ctc_periodic_refresh.cyclo ./trainctl/ctrl/ctc_periodic_refresh.d ./trainctl/ctrl/ctc_periodic_refresh.o ./trainctl/ctrl/ctc_periodic_refresh.su ./trainctl/ctrl/ctrl.cyclo ./trainctl/ctrl/ctrl.d ./trainctl/ctrl/ctrl.o ./trainctl/ctrl/ctrl.su ./trainctl/ctrl/ctrlLT.cyclo ./trainctl/ctrl/ctrlLT.d ./trainctl/ctrl/ctrlLT.o ./trainctl/ctrl/ctrlLT.su ./trainctl/ctrl/ctrlP.cyclo ./trainctl/ctrl/ctrlP.d ./trainctl/ctrl/ctrlP.o ./trainctl/ctrl/ctrlP.su ./trainctl/ctrl/detect_loco.cyclo ./trainctl/ctrl/detect_loco.d ./trainctl/ctrl/detect_loco.o ./trainctl/ctrl/detect_loco.su ./trainctl/ctrl/longtrain.cyclo ./trainctl/ctrl/longtrain.d ./trainctl/ctrl/longtrain.o ./trainctl/ctrl/longtrain.su ./trainctl/ctrl/longtrain4.cyclo ./trainctl/ctrl/longtrain4.d ./trainctl/ctrl/longtrain4.o ./trainctl/ctrl/longtrain4.su ./trainctl/ctrl/trace_train.cyclo ./trainctl/ctrl/trace_train.d ./trainctl/ctrl/trace_train.o ./trainctl/ctrl/trace_train.su
+	-$(RM) ./trainctl/ctrl/c3autoP.cyclo ./trainctl/ctrl/c3autoP.d ./trainctl/ctrl/c3autoP.o ./trainctl/ctrl/c3autoP.su ./trainctl/ctrl/cautoP.cyclo ./trainctl/ctrl/cautoP.d ./trainctl/ctrl/cautoP.o ./trainctl/ctrl/cautoP.su ./trainctl/ctrl/ctc_periodic_refresh.cyclo ./trainctl/ctrl/ctc_periodic_refresh.d ./trainctl/ctrl/ctc_periodic_refresh.o ./trainctl/ctrl/ctc_periodic_refresh.su ./trainctl/ctrl/ctrl.cyclo ./trainctl/ctrl/ctrl.d ./trainctl/ctrl/ctrl.o ./trainctl/ctrl/ctrl.su ./trainctl/ctrl/ctrlLT.cyclo ./trainctl/ctrl/ctrlLT.d ./trainctl/ctrl/ctrlLT.o ./trainctl/ctrl/ctrlLT.su ./trainctl/ctrl/ctrlP.cyclo ./trainctl/ctrl/ctrlP.d ./trainctl/ctrl/ctrlP.o ./trainctl/ctrl/ctrlP.su ./trainctl/ctrl/locomotives.cyclo ./trainctl/ctrl/locomotives.d ./trainctl/ctrl/locomotives.o ./trainctl/ctrl/locomotives.su ./trainctl/ctrl/longtrain.cyclo ./trainctl/ctrl/longtrain.d ./trainctl/ctrl/longtrain.o ./trainctl/ctrl/longtrain.su ./trainctl/ctrl/longtrain4.cyclo ./trainctl/ctrl/longtrain4.d ./trainctl/ctrl/longtrain4.o ./trainctl/ctrl/longtrain4.su ./trainctl/ctrl/trace_train.cyclo ./trainctl/ctrl/trace_train.d ./trainctl/ctrl/trace_train.o ./trainctl/ctrl/trace_train.su ./trainctl/ctrl/train2loco.cyclo ./trainctl/ctrl/train2loco.d ./trainctl/ctrl/train2loco.o ./trainctl/ctrl/train2loco.su
 
 .PHONY: clean-trainctl-2f-ctrl
 

9a5dff03fdc54a3f6363eedcf986f6725bfaf968
banyanshade-software <braun.daniel@free.fr>
2024-02-11 21:20:56 +0100

    add received_broadcast()

--- trainctl/uitrack/uigen.c
+++ trainctl/uitrack/uigen.c
@@ -24,12 +24,19 @@ void __attribute__((weak)) received_ui_gen(msg_64_t *m)
 {
 }
 
+void __attribute__((weak)) received_broadcast(msg_64_t *m)
+{
+}
 void uigen_run_tick(_UNUSED_ uint32_t notif_flags, _UNUSED_ uint32_t tick, _UNUSED_ uint32_t dt)
 {
     for (;;) {
         msg_64_t m;
         int rc = mqf_read_to_ui(&m);
         if (rc) break;
+        if (m.to == MA3_BROADCAST) {
+            received_broadcast(&m);
+            continue;
+        }
         if (m.to != MA3_UI_GEN) {
             itm_debug2(DBG_MSG, "notuigen", m.to, m.cmd);
             continue;

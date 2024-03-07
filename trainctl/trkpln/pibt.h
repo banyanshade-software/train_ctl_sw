d1df390abb73348b68f4e56d3f131b19f2926858
banyanshade-software <braun.daniel@free.fr>
2024-03-07 19:23:55 +0100

    add comment on pibt being inadequate for us

--- trainctl/trkpln/pibt.h
+++ trainctl/trkpln/pibt.h
@@ -11,7 +11,22 @@
 
 
 // https://kei18.github.io/pibt2/
-
+/*
+ the problem with PIBT is that it will backtrack only on a single time episode
+ path finding may arrive in a position where all trains are stuck on after the other
+ on a deadend track.
+ 
+ PIBT ensure reachability only on biconnected graph
+ (this is the case for warehouse, not for us)
+ model railway tracks includes many deadend tracks, usable to avoid collision, but
+ making the graph not biconnected
+ 
+ 
+ winPIBT
+ https://kei18.github.io/pibt/
+ https://arxiv.org/abs/1905.10149
+ 
+ */
 
 
 #include "../misc.h"
